d6f3e22e81b2ed22dbcac025dbad3e1849197283
Bogdan <ponomarenkooff@gmail.com>
2020-10-06 19:33:24 +0300

    Тест комит

---
+++ GameStore.WebUi/GameStore.Domain/Entities/ShippingDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Domain.Entities
+{
+    class ShippingDetails
+    {
+
+        [Required(ErrorMessage = "Укажите как вас зовут")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Вставьте первый адрес доставки")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Укажите город")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Укажите страну")]
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+
+    }
+}
